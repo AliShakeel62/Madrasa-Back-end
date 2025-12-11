6f2ac368dc896969cfd1a33c7b1a80e4cd1272b5
Ali Shakeel <alishakeel567567@gmail.com>
2025-12-11 18:01:58 +0500

    first commit

--- schema/student.schema.js
+++ schema/student.schema.js
@@ -7,12 +7,8 @@ const studentSchema = new moongose.Schema({
     fatherName: {type:String , required:true },
     addmissionDate: {type:Date , default:Date.now},
     address: String,
-<<<<<<< HEAD
     dateofBirth: Date,
     CreatedAt: { type: Date, default: Date.now }
-=======
-    dateofBirth: Date
->>>>>>> f2865aabf4467d273f2b204b193ba1fb2fa90de8
 })
 const Student = moongose.model('Students', studentSchema);
 module.exports = Student;
